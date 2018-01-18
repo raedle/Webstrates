7d09a423ce9b437202ebfcdae41a0bfacd79ac5b
Roman Rädle <roman.raedle@outlook.com>
2018-01-19 00:25:31 +0100

    Refactored nodeApproval: Expose configuration for nodeApproval to webstrate.config.nodeApproval object. Overriding node creation prototypes. Change nodeApproval type with url parameter (?verbose=true)

--- client/webstrates/nodeApproval.js
+++ client/webstrates/nodeApproval.js
@@ -1,9 +1,54 @@
 'use strict';
+const coreConfig = require('./coreConfig');
+const coreUtils = require('./coreUtils');
 const coreEvents = require('./coreEvents');
 
 const nodeApprovalModule = {
 	enabled: true
 };
+
+const approvalType = {
+	PROPERTY: 0,
+	ATTRIBUTE: 1
+};
+
+nodeApprovalModule.TYPE = new Proxy(approvalType, {
+	get: (target, name) => {
+		if (name in target) return target[name];
+		throw new Error(`Invalid approval type ${name}`);
+	}
+});
+
+coreConfig.nodeApproval = {
+	TYPE: nodeApprovalModule.TYPE,
+	type: nodeApprovalModule.TYPE.PROPERTY,
+	enable() {
+		nodeApprovalModule.enabled = true;
+	},
+	disable() {
+		nodeApprovalModule.enabled = false;
+	}
+};
+
+/******************************************/
+// @deprecated, will be removed soon
+const getParameterByName = function (name, url) {
+	if (!url) {
+		url = window.top.location.href;
+	}
+	name = name.replace(/[\[\]]/g, '\\$&');
+	let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+	let results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
+if (getParameterByName('verbose')) {
+	coreConfig.nodeApproval.type = approvalType.ATTRIBUTE;
+	console.log('verbose', true);
+}
+/******************************************/
 
 /**
  * Checks if the options parameter is an object, if it has the approved property, and
@@ -21,66 +66,59 @@ const isApproved = (options) => {
 	);
 };
 
-const approveElement = (element, options) => {
+const approveNode = (node, options) => {
 
-	if (!nodeApprovalModule.enabled && isApproved(options)) {
-		console.warn('document.createElement(tagName, { approved: true }) used, but not necessary');
-	}
+	if (node.nodeType !== Node.ELEMENT_NODE || !nodeApprovalModule.enabled) return;
 
-	if (nodeApprovalModule.enabled && !isApproved(options)) {
-		if (config.approveVerbose) {
-			element.setAttribute('not-approved', '');
-		}
-		else {
-			element.__isNotApproved__ = true;
+	if (!isApproved(options)) {
+		switch (coreConfig.nodeApproval.type) {
+			case approvalType.ATTRIBUTE:
+				node.setAttribute('not-approved', '');
+				break;
+			case approvalType.PROPERTY:
+				node.__isNotApproved__ = true;
+				break;
+			default:
+				break;
 		}
 	}
 };
 
-coreEvents.createEvent('enableApprove');
-coreEvents.createEvent('disableApprove');
-
-coreEvents.addEventListener('enableApprove', () => {
-	// console.log('enable approve');
-	// nodeApprovalModule.enabled = true;
-}, coreEvents.PRIORITY.IMMEDIATE);
-
-coreEvents.addEventListener('disableApprove', () => {
-	// console.log('disable approve');
-	// nodeApprovalModule.enabled = false;
-}, coreEvents.PRIORITY.IMMEDIATE);
-
-coreEvents.addEventListener('populatedBeforeScriptExecution', () => {
+coreEvents.addEventListener('beforeExecuteScripts', () => {
 
 	console.log('execute node approval');
-
-	const _createElement = Document.prototype.createElement;
-	Document.prototype.createElement = function (tagName, options, ...unused) {
-		let element = _createElement.call(this, tagName, options, unused);
-		approveElement(element, options);
-		return element;
-	};
 
 	const _createElementNS = Document.prototype.createElementNS;
 	Document.prototype.createElementNS = function (namespaceURI, qualifiedName, options, ...unused) {
 		let element = _createElementNS.call(this, namespaceURI, qualifiedName, options, unused);
-		approveElement(element, options);
+		approveNode(element, options);
 		return element;
 	};
 
-	// document.___createElementNS = document.createElementNS;
-	// document.createElementNS = function(namespaceURI, qualifiedName, options, ...unused) {
-	// 	let element = document.___createElementNS(namespaceURI, qualifiedName, options, unused);
-	// 	approveElement(element, options);
-	// 	return element;
-	// };
+	const _createElement = Document.prototype.createElement;
+	Document.prototype.createElement = function (tagName, options, ...unused) {
+		let element = _createElement.call(this, tagName, options, unused);
+		approveNode(element, options);
+		return element;
+	};
 
-	// document.___createElement = document.createElement;
-	// document.createElement = function(tagName, options, ...unused) {
-	// 	let element = document.___createElement(tagName, options, unused);
-	// 	approveElement(element, options);
-	// 	return element;
-	// };
+	const importNode = Document.prototype.importNode;
+	Document.prototype.importNode = function (externalNode, deep, options, ...unused) {
+		var element = importNode.call(this, externalNode, deep, ...unused);
+		coreUtils.recursiveForEach(element, childNode => {
+			approveNode(childNode, options);
+		});
+		return element;
+	};
+
+	const cloneNode = Node.prototype.cloneNode;
+	Node.prototype.cloneNode = function (deep, options, ...unused) {
+		var element = cloneNode.call(this, deep, ...unused);
+		coreUtils.recursiveForEach(element, childNode => {
+			approveNode(childNode, options);
+		});
+		return element;
+	};
 }, coreEvents.PRIORITY.IMMEDIATE);
 
 module.exports = nodeApprovalModule;
